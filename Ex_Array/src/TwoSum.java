c9a8d879e546bbb5d48487906761d58d01a2129f
Nguyễn Xuân Tuân <tuannguyenxuancv@gmail.com>
2022-11-04 20:36:51 +0700

    update TwoSum

--- Ex_Array/src/TwoSum.java
+++ Ex_Array/src/TwoSum.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class TwoSum {
     public static void main(String[] args) {
         int[] a = {2,5,7,15};
         int target = 9;
 
-        twoSum(a,target);
+       Arrays.stream(twoSum(a, target)).forEach(System.out::println);
     }
 
     public static int[] twoSum(int[] a, int target) {
@@ -11,12 +13,10 @@ public class TwoSum {
         for(int i=0;i<a.length;i++){
             for(int j=i+1;j<a.length;j++){
                 if((a[i]+a[j]) == target){
-                    arr[0] = i;
-                    arr[1]= j;
+                    return new int[] {i,j};
                 }
             }
         }
-
-        return arr;
+        throw new RuntimeException();
     }
 }
